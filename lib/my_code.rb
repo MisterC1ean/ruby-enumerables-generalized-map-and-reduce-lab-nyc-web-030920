07c86ce82057dc250b8e690278c0080480874e47
Robert Keller <robert_keller44@icloud.com>
2020-01-15 02:18:15 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,10 @@
 # Your Code Here
 def map(source_array)
-  
+  #map returns a new array of manipulated data
+  new = []
+  i = 0
+  while i < source_array.length do
+    yield
+    counter += 1
+  end
 end
